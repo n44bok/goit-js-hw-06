99dce0510a4e9c61d117f50e571ab586186051ca
Oleksandr Romanov <vamromanov@gmail.com>
2022-02-07 22:42:56 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,17 +1,20 @@
-const inputEl = document.querySelector('#font-size-control'),
-   
+
+const refs = {
+    input: document.querySelector('#name-input'),
+    output: document.querySelector('#name-output'),
+}   
 
 refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
     if (event.currentTarget.value === "") {
-        refs.nameLabel.textContent = 'Anonymous';
+        refs.output.textContent = 'Anonymous';
     } else {
-        refs.nameLabel.textContent = event.currentTarget.value;
-    }
+        refs.output.textContent = event.currentTarget.value;
+    };
 
     
     
-}
+};
 
 
